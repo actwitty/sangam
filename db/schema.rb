7a2966aeafdc596d1a9e7531d8fe905f4a977cad
Alok Srivastava <alok@lemonbag.com>
2011-05-04 15:39:30 +0530

    added entity_id in hub

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110502113130) do
+ActiveRecord::Schema.define(:version => 20110504100106) do
 
   create_table "activities", :force => true do |t|
     t.string   "activity_name"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20110502113130) do
     t.integer  "activity_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "entity_id"
   end
 
   create_table "likes", :force => true do |t|
