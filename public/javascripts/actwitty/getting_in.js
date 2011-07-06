bf1bdc1a1231f3e3dc1954e92bb69169766b68dd
Sudhanshu Saxena <sudhanshu@lemonbag.com>
2011-07-06 20:43:38 +0530

    getting in added

---
+++ public/javascripts/actwitty/getting_in.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $(function() {
+    $(".actwittysignup").live('click', function() {
+      var auth_key = $('#foreign_auths').find('input[name="auth_key"]').val();
+      var auth_uid =  $('#foreign_auths').find('input[name="auth_uid"]').val();
+      var auth_provider =$('#foreign_auths').find('input[name="auth_provider"]').val();
+      $(this).closest('form').find('input[name="user[provider]"]').val(auth_provider);
+      $(this).closest('form').find('input[name="user[key]"]').val(auth_key);
+      $(this).closest('form').find('input[name="user[uid]"]').val(auth_uid);
+
+      $.ajax({
+        url: "/users",
+        type: 'POST',
+        data: $(this).closest('form').serialize()
+     
+      });
+      return false;
+  });
+
+
+  $(".actwittysignin").live('click', function() {
+      var auth_key = $('#foreign_auths').find('input[name="auth_key"]').val();
+      var auth_uid =  $('#foreign_auths').find('input[name="auth_uid"]').val();
+      var auth_provider =$('#foreign_auths').find('input[name="auth_provider"]').val();
+      $(this).closest('form').find('input[name="user[provider]"]').val(auth_provider);
+      $(this).closest('form').find('input[name="user[key]"]').val(auth_key);
+      $(this).closest('form').find('input[name="user[uid]"]').val(auth_uid);
+
+      $.ajax({
+        url: "/users/sign_in",
+        type: 'POST',
+        data: $(this).closest('form').serialize()
+     
+      });
+      return false;
+    });
+
+  });
+
