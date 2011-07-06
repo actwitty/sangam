8c74b66379b53c559451d18c100199e1a87290b4
Alok Srivastava <alok@lemonbag.com>
2011-07-06 19:59:26 +0530

    production modification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Sangam::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'simple-mountain-557.heroku.com' }
     
   config.action_mailer.delivery_method = :smtp
 
