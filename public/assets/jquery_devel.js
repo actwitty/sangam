a3e5b6b6537726b9a7264a8b42ecccb9bfb8a8cd
Alok Srivastava <alok@lemonbag.com>
2011-07-08 00:41:10 +0530

    assets at Friday 08, 2011

--- public/assets/jquery_devel.js
+++ public/assets/jquery_devel.js
@@ -1154,6 +1154,55 @@ $.fn.metadata = function( opts ){
 };
 
 })(jQuery);
+$(document).ready(function(){
+
+    $(".actwittysignup").live('click', function() {
+      var auth_key = $('#foreign_auths').find('input[name="auth_key"]').val();
+      var auth_uid =  $('#foreign_auths').find('input[name="auth_uid"]').val();
+      var auth_provider =$('#foreign_auths').find('input[name="auth_provider"]').val();
+      $(this).closest('form').find('input[name="user[provider]"]').val(auth_provider);
+      $(this).closest('form').find('input[name="user[key]"]').val(auth_key);
+      $(this).closest('form').find('input[name="user[uid]"]').val(auth_uid);
+
+      $.ajax({
+        url: "/users",
+        type: 'POST',
+        data: $(this).closest('form').serialize(),
+        beforeSend: function(){
+          $('#LoadingDiv').css('display','block'); 
+        },
+        success: function(){
+          
+          $('#LoadingDiv').css('display','none');
+        },
+      });
+      return false;
+  });
+
+
+  $(".actwittysignin").live('click', function() {
+      var auth_key = $('#foreign_auths').find('input[name="auth_key"]').val();
+      var auth_uid =  $('#foreign_auths').find('input[name="auth_uid"]').val();
+      var auth_provider =$('#foreign_auths').find('input[name="auth_provider"]').val();
+      $(this).closest('form').find('input[name="user[provider]"]').val(auth_provider);
+      $(this).closest('form').find('input[name="user[key]"]').val(auth_key);
+      $(this).closest('form').find('input[name="user[uid]"]').val(auth_uid);
+
+      $.ajax({
+        url: "/users/sign_in",
+        type: 'POST',
+        data: $(this).closest('form').serialize(),
+        beforeSend: function(){
+          $('#LoadingDiv').css('display','block'); 
+        },
+        success: function(){
+          $('#LoadingDiv').css('display','none');
+        },
+      });
+      return false;
+    });
+
+  });
 /*!
  * jQuery UI 1.8.13
  *
