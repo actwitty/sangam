b91e0456a04d2c66d1d6be4cb97b57fd7c1ab85e
Anand <anand@Anand-PC.(none)>
2011-07-09 15:36:11 +0530

    entity sanity check

--- lib/entities/query_manager.rb
+++ lib/entities/query_manager.rb
@@ -3,6 +3,9 @@ module QueryManager
 require 'zemanta_wrapper'
 require 'freebase_wrapper'
 
+ ENGLISH_STOP_WORDS= %w{a about above across after afterwards again against all almost alone along already also although always am among amongst amoungst amount an and another any anyhow anyone anything anyway anywhere are around as at back be became because become becomes becoming been before beforehand behind being below beside besides between beyond bill both bottom btw but by call can cannot cant co computer con could couldnt cry de describe detail do done down due during each eg eight either eleven else elsewhere empty enough etc even ever every everyone everything everywhere except few fifteen fify fill find fire first five for former formerly forty found four from front full further get give go had has hasnt have he hence her here hereafter hereby herein hereupon hers him his how however hundred i ie if in inc indeed interest into is it its keep last latter latterly least less ltd made many may me meanwhile might mill mine more moreover most mostly move much must my name namely neither never nevertheless next nine no nobody none noone nor not nothing now nowhere of off often on once one only onto or other others otherwise our ours ourselves out over own part per perhaps please put rather re same see seem seemed seeming seems serious several she should show side since sincere six sixty so some somehow someone something sometime sometimes somewhere still such system take ten than that the their them themselves then there thereafter thereby therefore therein thereupon these they thick thin third this those though three through throughout thru thus to together too top toward towards twelve twenty two un under until up upon us very via was we well were what whatever when whenever where whereafter whereas whereby wherein whereupon wherever whether which while whither who whoever whole whom whose why will with within without would yet you your yours yourself yourselves}
+
+
 	def titlecase str
       non_capitalized = %w{of etc and by the for on is at to but nor or a via}
       str.gsub(/\b[a-z]+/){ |w| non_capitalized.include?(w) ? w : w.capitalize  }.sub(/^[a-z]/){|l| l.upcase }.sub(/\b[a-z][^\s]*?$/){|l| l.capitalize }
@@ -20,13 +23,15 @@ require 'freebase_wrapper'
     		next
      	elsif text.casecmp(item["name"])==0
 				entity=item
-				break	
+				break
+=begin
 			else
 				alt=item["/common/topic/alias"].select {|l| text.casecmp(l)==0}
 				unless alt.empty?
 					entity=item
 					break
 				end
+=end
  			end
 		end
 		return entity
@@ -67,9 +72,9 @@ require 'freebase_wrapper'
 			entities << result
 			markupName << item["anchor"]
 #===================================================
-			puts "Markup: #{item["anchor"]}"
-			puts result
-			print "\n"
+			#puts "Markup: #{item["anchor"]}"
+			#puts result
+			#print "\n"
 #===================================================
 		end
 		return entities
@@ -89,9 +94,9 @@ require 'freebase_wrapper'
 					result=freebase_search item["name"]
 					entities << result
 #=======================================================
-					puts "Suggestion:#{item["name"]}"
-				  puts result
-   				print "\n"
+					#puts "Suggestion:#{item["name"]}"
+				  #puts result
+   				#print "\n"
 #=======================================================
 				end
 			end	
@@ -99,7 +104,35 @@ require 'freebase_wrapper'
 	
 	end
 
-  
+def entity_sanity_check(query,entities)
+    ent_count=entities.length
+		ent_index=0
+  	while(ent_index<ent_count)
+			validation=true
+      query.scan(/\w*[\s-]*\b#{entities[ent_index]["name"]}\b[\s-]*\w*/i).each do |item|
+        words=item.sub(/#{entities[ent_index]["name"]}/i,'')
+				if words.include?('-')
+					validation=false
+        else
+					words.scan(/\w+/).each do |word|
+						validation=ENGLISH_STOP_WORDS.include?(word)
+						break unless validation
+			  	end
+        end
+        break if validation
+      end
+
+			unless validation
+				entities.delete_at(ent_index)
+				ent_count -=1
+			else
+				ent_index +=1
+      end
+
+    end
+  end
+
+
 
 	def entities_adapter(query)
     markupName=[]
@@ -108,7 +141,7 @@ require 'freebase_wrapper'
     formatted_query=titlecase(query)+"\n"+query
     zemanta_get_markup_entity(formatted_query,entities,markupName)
     zemanta_get_suggestion_entity(formatted_query,entities,markupName)
-    
+    entity_sanity_check(query,entities)
 		return entities
 
   end
