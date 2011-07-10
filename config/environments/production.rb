ad91ce2c7f6eed8179c01da38601bae36d8f85a1
Alok Srivastava <alok@lemonbag.com>
2011-07-11 00:09:57 +0530

    modified the domain of defualt host in production.rb to www.actwitty.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Sangam::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'actwitty.com' }
+  config.action_mailer.default_url_options = { :host => 'www.actwitty.com' }
     
   config.action_mailer.delivery_method = :smtp
 
