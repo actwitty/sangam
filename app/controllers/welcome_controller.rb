6523cb2e739449a50726ccf59d139b3f4880fb9c
Alok Srivastava <alok@actwitty.com>
2011-08-26 17:24:41 +0530

    static page cache

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     if user_signed_in?
       redirect_to :controller => "home", :action => "show"
       #redirect_to :controller => "home", :action => "alpha"
+    else
+      response.headers['Cache-Control'] = 'public, max-age=300'
     end
   end
 
