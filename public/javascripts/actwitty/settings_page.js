600400b8c65f33d9cfbd0763a6cfd6c6e6b79962
Sudhanshu Saxena <sudhanshu@actwitty.com>
2011-10-05 15:09:10 +0530

    added

--- public/javascripts/actwitty/settings_page.js
+++ public/javascripts/actwitty/settings_page.js
@@ -12,5 +12,34 @@ function aw_notify_profile_image(main, thumb){
 
   $("#profile_profile_photo_l").val(main);   
   $("#profile_profile_photo_s").val(thumb);
-  $("#user_settings_image").attr("src", main);
+  $("#user_settings_image").attr("src", thumb);
 }
+
+
+function preview(img, selection) {
+    if (!selection.width || !selection.height)
+        return;
+    
+    var scaleX = 100 / selection.width;
+    var scaleY = 100 / selection.height;
+
+    $('#user_settings_image_snap img').css({
+        width: Math.round(scaleX * 50),
+        height: Math.round(scaleY * 50),
+        marginLeft: -Math.round(scaleX * selection.x1),
+        marginTop: -Math.round(scaleY * selection.y1)
+    });
+
+    $('#x1').val(selection.x1);
+    $('#y1').val(selection.y1);
+    $('#x2').val(selection.x2);
+    $('#y2').val(selection.y2);
+    $('#w').val(selection.width);
+    $('#h').val(selection.height);    
+}
+
+aw_user_image_selector_init() {
+    $('#user_settings_image').imgAreaSelect({ aspectRatio: '1:1', handles: true,
+        fadeSpeed: 200, onSelectChange: preview });
+};
+
