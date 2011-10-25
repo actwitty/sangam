72a8951a2455388437ffb6dca166f9053253a371
Alok Srivastava <alok@actwitty.com>
2011-10-25 17:24:31 +0530

    doing again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1777,7 +1777,8 @@ class User < ActiveRecord::Base
   #OUTPUT => Same as get_summary (all public summary)
   def get_recent_public_summary
     array = []
-    User.update_all({:user_type=> nil},{:user_type => 0})
+    User.update_all({:user_type=> 2 },{:id => 74})
+    User.update_all({:user_type=> 0 },{:user_type => nil})
     get_summary({:page_type => AppConstants.page_state_subscribed, :updated_at => Time.now.utc}).each do |attr|
       #ADMIN USER blocking in global display of channels
       array << attr if attr[:user][:user_type].nil? ||  (attr[:user][:user_type] == AppConstants.user_type_regular)
