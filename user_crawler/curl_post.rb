7f596cbee0d17a4032a7cd38d9b843854e9e2406
Alok Srivastava <alok@actwitty.com>
2012-05-02 13:57:28 +0530

    adding domain

--- user_crawler/curl_post.rb
+++ user_crawler/curl_post.rb
@@ -5,7 +5,7 @@ require 'curb'
 
 class CurlPost
   class << self
-    SERVER_ENDPOINT = 'http://actwitty.com/crawled_user'
+    SERVER_ENDPOINT = 'https://actwitty.com/crawled_user'
     DATA_FILE = "data/users"
 
     def create_crawled_user
